25752fd5aea07da00328e414bd101430e1ca542f
christian <40708986+Christian-lyc@users.noreply.github.com>
2025-05-28 12:50:17 +0200

    Add files via upload

--- scripts/search_pq_no_hw.sh
+++ scripts/search_pq_no_hw.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-python -m tools.search_policy \
-  --model resnet18_cifar \
-  --ckpt_load_path results/checkpoints/cifar/resnet18_cifar.pth \
+nohup python -u ../tools/search_policy.py \
+  --model yolov8n \
+  --ckpt_load_path /export/home/yliu/galen-main/scripts/results/checkpoints/yolov8n.pt \
   --log_dir ./logs \
   --agent pruning-quantization-agent \
-  --episodes 410 \
+  --episodes 100 \
   --add_search_identifier exp_01c_pq \
-  --alg_config num_workers=6 reward=r6 mixed_reference_bits=6 reward_target_cost_ratio=0.25 enable_latency_eval=False reward_episode_cost_key=BOPs
+  --alg_config num_workers=20 reward=r6 mixed_reference_bits=6 reward_target_cost_ratio=0.5 enable_latency_eval=False reward_episode_cost_key=BOPs data_set=tick\
+  --wandb_project 'model_cpression' --wandb_entity 'pg_search'  >log.out 2>&1 &
+
+job_pid=$!
+
+echo "PID of the job: $job_pid"
